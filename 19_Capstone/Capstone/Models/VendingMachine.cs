e03af43f495592788fee0d880d9c017c7b15ea11
Seth Boyle <s.boyle987@gmail.com>
2020-06-04 12:25:01 -0400

    Added new class VendingMachine, added method inside that class called Restock

---
+++ 19_Capstone/Capstone/Models/VendingMachine.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Capstone.Models.Items;
+
+namespace Capstone.Models
+{
+    public class VendingMachine
+    {
+        public decimal Balance { get; set; } = 0.00M;
+
+        public List<Item> Inventory { get; set; }
+
+        public VendingMachine()
+        {
+            //restock
+        }
+
+        public void Restock()
+        {
+            //set path to get file from
+            string path = @"C:\Users\Student\git\c-module-1-capstone-team-8\19_Capstone\vendingmachine.csv"; //TODO 01 CHANGE TO RELATIVE
+            //open file
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    //read line by line
+                    string singleItem = sr.ReadLine();
+
+                    //split by the | delimter to array
+                    string[] singleItemContents = singleItem.Split("|");
+
+                    //reference by index and store values in temp variable
+                    string tempName = singleItemContents[1];
+                    string tempSlot = singleItemContents[0];
+                    string tempItemType = singleItemContents[3];
+                    decimal tempPrice = decimal.Parse(singleItemContents[2]);
+
+                    //create a new object to assign the values
+                    if (tempItemType == "Chip")
+                    {
+                        Inventory.Add(new Chip(tempName, tempSlot, tempPrice));
+                    }
+                    if (tempItemType == "Drink")
+                    {
+                        Inventory.Add(new Drink(tempName, tempSlot, tempPrice));
+                    }
+                    if (tempItemType == "Gum")
+                    {
+                        Inventory.Add(new Gum(tempName, tempSlot, tempPrice));
+                    }
+                    if (tempItemType == "Candy")
+                    {
+                        Inventory.Add(new Candy(tempName, tempSlot, tempPrice));
+                    }
+
+
+                }
+            }
+            
+            
+            
+
+
+        }
+    }
+}
