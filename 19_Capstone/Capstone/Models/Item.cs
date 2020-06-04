bf3fe1bef529dc08f39c5023a9a61c2d4ef9b127
Seth Boyle <s.boyle987@gmail.com>
2020-06-04 11:10:17 -0400

    Created Item Class

--- 19_Capstone/Capstone/Models/Item.cs
+++ 19_Capstone/Capstone/Models/Item.cs
@@ -4,7 +4,22 @@ using System.Text;
 
 namespace Capstone.Models
 {
-    class Class1
+    abstract class Item
     {
+        public string Name { get;  } //readonly
+        public string SlotLocation { get;  } //readonly
+
+        public decimal Price { get; } //readonly
+
+        public int QuantityAvailable { get; set; }
+
+        public Item(string name, string slotLocation, decimal price, int quantityAvailable )
+        {
+            this.Name = name;
+            this.SlotLocation = slotLocation;
+            this.Price = price;
+            this.QuantityAvailable = quantityAvailable;
+        }
+
     }
 }
